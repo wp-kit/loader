e2bda09e9a2c2991ab2f79f5f24934803bf993ce
terence1990 <terence@creativelittledots.co.uk>
2017-08-18 11:29:36 +0100

    Merged repo functions

--- helpers.php
+++ helpers.php
@@ -1,14 +1,20 @@
 <?php
 
 	use Illuminate\Container\Container;
+	use Illuminate\Support\Facades\Input;
 
 	if( ! defined( 'DS' ) ) {
 		
 		define( 'DS', DIRECTORY_SEPARATOR );
 		
 	}
+	
+	/*----------------------------------------------*\
+    	#SERVICE WRAPPERS
+    \*----------------------------------------------*/
 
-	if (!function_exists('app')) {
+	if ( ! function_exists('app') ) {
+		
 	    /**
 	     * Helper function to quickly retrieve an instance.
 	     *
@@ -24,9 +30,11 @@
 	        }
 	        return Container::getInstance()->make($abstract, $parameters);
 	    }
+	    
 	}
 
-	if (! function_exists('session')) {
+	if ( ! function_exists('session') ) {
+		
 	    /**
 	     * Get / set the specified session value.
 	     *
@@ -46,9 +54,11 @@
 	        }
 	        return app('session')->get($key, $default);
 	    }
+	    
 	}
 
-	if (!function_exists('view')) {
+	if ( ! function_exists('view') ) {
+		
 	    /**
 	     * Helper function to build views.
 	     *
@@ -66,9 +76,15 @@
 	        }
 	        return $factory->make($view, $data, $mergeData)->render();
 	    }
+	    
 	}
+	
+	/*----------------------------------------------*\
+    	#PATHS
+    \*----------------------------------------------*/
 
 	if( ! function_exists('resources_path') ) {
+		
 		/**
 	     * Gets the resources path
 	     *
@@ -83,14 +99,17 @@
 		    }
 		    return $root . ( $path ? DS . $path : '' ) . ltrim( ( $file ? DS . $file : '' ), DS );
 	    }
+	    
 	}
 	
 	if( ! function_exists('storage_path') ) {
+		
 		/**
 	     * Gets the storage path
 	     *
 	     * @return string
 	     */
+	     
 	    function storage_path($file = '')
 	    {
 		    if( function_exists('themosis_path') ) {
@@ -99,9 +118,11 @@
 			    return resources_path('storage', $file);
 		    }
 	    }
+	    
 	}
 	
 	if( ! function_exists('config_path') ) {
+		
 		/**
 	     * Gets the storage path
 	     *
@@ -111,9 +132,11 @@
 	    {
 		    return resources_path('config', $file);
 	    }
+	    
 	}
 	
 	if( ! function_exists('view_path') ) {
+		
 		/**
 	     * Gets the view path
 	     *
@@ -123,93 +146,25 @@
 	    {
 		    return resources_path('views', $file);
 	    }
-	}
-
-	if( ! function_exists( 'config_path' ) ) {
-		
-		function config_path( $file = '' ) {
-			
-		    if( function_exists('themosis_path') ) {
-			    $path = themosis_path('theme.resources') . DS . 'config';
-		    } else {
-			    $path = get_stylesheet_directory() . DS . 'resources' . DS . 'config';
-		    }
-		    return $path . ltrim( ( $file ? DS . $file : '' ), DS );
-			
-		}
-		
-	}
-	
-	if( ! function_exists('resources_path') ) {
-		/**
-	     * Gets the resources path
-	     *
-	     * @return string
-	     */
-	    function resources_path($path = '', $file = '')
-	    {
-		    if( function_exists('themosis_path') ) {
-			    $path = themosis_path('theme' . ( $path ? '.' . $path : ''));
-		    } else {
-			    $path = get_stylesheet_directory() . DS . 'resources' . ( $path ? DS . $path : '' );
-		    }
-		    return $path . ltrim( ( $file ? DS . $file : '' ), DS );
-	    }
-	}
-	
-	if ( ! function_exists('get_asset') ) {
-    
-	    function get_asset( $file, $server_path = false ) {
-		    
-		    if( file_exists( get_stylesheet_directory() . DS . $dir . DS . $file ) ) {
-		            
-	            $path = $server_path ? get_stylesheet_directory() : get_stylesheet_directory_uri();
-                
-                return $path . DS . $dir . DS . $file;
-                
-            }
-		    
-	    }
 	    
 	}
-
-	if (!function_exists('view')) {
-	    /**
-	     * Helper function to build views.
-	     *
-	     * @param string $view      The view relative path, name.
-	     * @param array  $data      Passed data.
-	     * @param array  $mergeData
+		
+	if( ! function_exists( 'shortcode_path' ) ) {
+		
+		/**
+	     * Gets the shortcodes path
 	     *
 	     * @return string
 	     */
-	    function view($view = null, array $data = [], array $mergeData = [])
-	    {
-	        $factory = app('view');
-	        if (func_num_args() === 0) {
-	            return $factory;
-	        }
-	        return $factory->make($view, $data, $mergeData)->render();
-	    }
-	}
-	
-	if( ! function_exists( 'shortcode_path' ) ) {
-		
-		function shortcode_path( $file = '' ) {
-			
-		    if( function_exists('themosis_path') ) {
-			    $path = themosis_path('theme.resources') . DS . 'shortcodes';
-		    } else {
-			    $path = get_stylesheet_directory() . DS . 'resources' . DS . 'shortcodes';
-		    }
-		    return $path . ltrim( ( $file ? DS . $file : '' ), DS );
-			
+		function shortcode_path( $file = '' ) 
+		{
+			return resources_path('shortcodes', $file);
 		}
 		
 	}
 
 	/*----------------------------------------------*\
-	#IS WP_LOGIN
+		#IS WP_LOGIN
     \*----------------------------------------------*/
 
     if ( ! function_exists('is_wp_login') ) {
@@ -225,8 +180,6 @@
 	/*----------------------------------------------*\
     	#ROUTES
     \*----------------------------------------------*/
-    
-    use Themosis\Facades\Input;
 	
 	if( ! function_exists('get_current_url') ) {
 		
@@ -331,9 +284,23 @@
     
     if ( ! function_exists('get_asset') ) {
     
-	    function get_asset($file, $server_path = false) {
+	    function get_asset( $file ) {
 		    
-		    return container('asset.finder')->find( $file );
+		    if( container()->bound('asset.finder') ) {
+		    
+		    	return container('asset.finder')->find( $file );
+		    	
+		    } else {
+			    
+			    if( file_exists( get_stylesheet_directory() . DS . $dir . DS . $file ) ) {
+	                
+	                return get_stylesheet_directory_uri() . DS . $dir . DS . $file;
+	                
+	            }
+			    
+		    }
+		    
+		    return null;
 	        
 	    }
 	    
@@ -345,7 +312,7 @@
     
     if ( ! function_exists('the_asset') ) {
     
-	    function the_asset($file) {
+	    function the_asset( $file ) {
 	        
 	        if( $asset = get_asset( $file ) ) {
 		        
